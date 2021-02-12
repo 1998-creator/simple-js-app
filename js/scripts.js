fd7c619e3146908a785dd7f7f93f5caad230f268
Payton  Provo <paytonprovo1@gmail.com>
2021-02-12 17:53:04 -0500

    Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let myfavoriteFood= 'steak';
-document.write(myfavoriteFood);
+let pokemonList = [
+    {name: "Bulbasaur", height: 7, types: ['grass', 'poison']},
+    {name: "Squirtle", height: 5, types:['water']},
+    {name: "Pikachu", height: 4, types:['electric']},
+    {name: "Caterpie", height: 3, types:['bug']}
+];
